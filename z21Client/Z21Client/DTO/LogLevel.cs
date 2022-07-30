a3357ed8b72be64b3fce6e3d30b33a0d583b25ca
Jakob Eichberger <jakob.eichberger@gmx.net>
2022-07-30 11:43:19 +0200

    Created LogLevel cs. Using NLog LogLeves definitions.

---
+++ z21Client/Z21Client/DTO/LogLevel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z21Client.DTO
+{
+    internal enum LogLevel
+    {
+        /// <summary>
+        /// Most verbose level.Used for development and seldom enabled in production.Ex.Request-payload, Response-payload, Begin-method-X or End-method-X
+        /// </summary>
+        Trace = 0,
+
+        /// <summary>
+        /// Debugging the application behavior from internal events of interest.Ex.Executed query, User authenticated, Session expired
+        /// </summary>
+        Debug = 1,
+
+        /// <summary>
+        /// Information that highlights progress or application lifetime events.
+        /// </summary>
+        Info = 2,
+
+        /// <summary>
+        /// Warnings about validation issues or temporary failures that can be recovered.
+        /// </summary>
+        Warn = 3,
+
+        /// <summary>
+        /// Errors where functionality has failed or Exception have been caught.	
+        /// </summary>
+        Error = 4,
+
+        /// <summary>
+        /// Most critical level.Application is about to abort.
+        /// </summary>
+        Fatal = 5,
+    }
+}
