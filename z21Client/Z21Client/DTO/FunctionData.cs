0941d57350ce158780084d2051326305c2ec56d5
Jakob Eichberger <jakob.eichberger@gmx.net>
2022-07-30 11:20:02 +0200

    Adding FunctionData.cs to hold data about a specific function.

---
+++ z21Client/Z21Client/DTO/FunctionData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainDatabase.Z21Client.DTO;
+using TrainDatabase.Z21Client.Enums;
+
+namespace Z21Client.DTO
+{
+    internal class FunctionData
+    {
+        public int LokAdresse { get; set; }
+
+        public int FunctionAdress { get; set; }
+
+        public ToggleType ToggleType { get; set; }
+    }
+}
