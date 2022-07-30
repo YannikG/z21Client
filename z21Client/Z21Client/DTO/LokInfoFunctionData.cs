35e794327a5afa78ae42fb283db60170f534fe00
Jakob Eichberger <jakob.eichberger@gmx.net>
2022-07-30 11:16:38 +0200

    Added z21Client from the TrainDatabase repo.

---
+++ z21Client/Z21Client/DTO/LokInfoFunctionData.cs
@@ -0,0 +1,13 @@
+﻿using TrainDatabase.Z21Client.Enums;
+
+namespace TrainDatabase.Z21Client.DTO
+{
+    public class LokInfoFunctionData
+    {
+        public int FunctionAddress { get; set; }
+
+        public LokAdresse LokAdresse { get; set; } = default!;
+
+        public ToggleType ToggleType { get; set; }
+    }
+}
---
+++ z21Client/Z21Client/Events/GetLocoFunctionInfoEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TrainDatabase.Z21Client.DTO;
+
+namespace TrainDatabase.Z21Client.Events
+{
+    public class GetLocoFunctionInfoEventArgs : EventArgs
+    {
+        public LokInfoFunctionData Data;
+
+        public GetLocoFunctionInfoEventArgs(LokInfoFunctionData data) : base()
+        {
+            Data = data;
+        }
+    }
+}
