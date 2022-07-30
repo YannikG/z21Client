621d38f5ccaa211a8fc217716e1b6539c40dfead
Jakob Eichberger <jakob.eichberger@gmx.net>
2022-07-30 15:16:32 +0200

    Adding a constructors to FunctionData.cs

--- z21Client/Z21Client/DTO/FunctionData.cs
+++ z21Client/Z21Client/DTO/FunctionData.cs
@@ -4,6 +4,27 @@ namespace Z21.DTO
 {
     public class FunctionData
     {
+        public FunctionData(LokAdresse lokAdresse, int functionAdress, ToggleType toggleType)
+        {
+            LokAdresse = lokAdresse;
+            FunctionAdress = functionAdress;
+            ToggleType = toggleType;
+        }
+
+        public FunctionData(int lokAdresse, int functionAdress, ToggleType toggleType)
+        {
+            LokAdresse = new(lokAdresse);
+            FunctionAdress = functionAdress;
+            ToggleType = toggleType;
+        }
+
+        public FunctionData(long lokAdresse, int functionAdress, ToggleType toggleType)
+        {
+            LokAdresse = new(lokAdresse);
+            FunctionAdress = functionAdress;
+            ToggleType = toggleType;
+        }
+
         public LokAdresse LokAdresse { get; set; } = new(-1);
 
         public int FunctionAdress { get; set; }
