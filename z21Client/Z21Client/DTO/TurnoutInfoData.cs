3c84d73dc4c26ab4e8ea9ef92332fe0986974b78
Jakob Eichberger <jakob.eichberger@gmx.net>
2022-08-07 22:23:57 +0200

    Adding turnout address, infodata und sate classes/enums.

---
+++ z21Client/Z21Client/DTO/TurnoutInfoData.cs
@@ -0,0 +1,44 @@
+﻿/* Z21 - C#-Implementierung des Protokolls der Kommunikation mit der digitalen
+ * Steuerzentrale Z21 oder z21 von Fleischmann/Roco
+ * ---------------------------------------------------------------------------
+ * Datei:     z21.cs
+ * Version:   16.06.2014
+ * Besitzer:  Mathias Rentsch (rentsch@online.de)
+ * Lizenz:    GPL
+ *
+ * Die Anwendung und die Quelltextdateien sind freie Software und stehen unter der
+ * GNU General Public License. Der Originaltext dieser Lizenz kann eingesehen werden
+ * unter http://www.gnu.org/licenses/gpl.html.
+ * 
+ */
+
+using Z21.Enums;
+
+namespace Z21.DTO
+{
+    public class TurnoutInfoData
+    {
+
+        public TurnoutInfoData(int adresse, State state)
+        {
+            Adresse = new(adresse);
+            State = state;
+        }
+
+        public TurnoutInfoData(long adresse, State state)
+        {
+            Adresse = new(adresse);
+            State = state;
+        }
+
+        public TurnoutInfoData(TurnoutAdresse adresse, State state)
+        {
+            Adresse = adresse;
+            State = state;
+        }
+
+        public TurnoutAdresse Adresse { get; private set; }
+
+        public State State { get; private set; }
+    }
+}
